23bf60c7d2a71b646f2658e604a97f1021262ec7
ludovicspina <ludovic.spina@proton.me>
2025-03-10 14:36:39 +0100

    edition data command

--- features/data/data.js
+++ features/data/data.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const User = require('../../database/models/user')(sequelize, Sequelize.DataTypes);
 const Message = require('../../database/models/message')(sequelize, Sequelize.DataTypes);
 const VoiceConnection = require('../../database/models/voiceConnection')(sequelize, Sequelize.DataTypes);
-const Reaction = require('../../database/models/reaction')(sequelize, Sequelize.DataTypes); // Nouveau modèle pour les réactions
+const Reaction = require('../../database/models/reaction')(sequelize, Sequelize.DataTypes); // Ajoutez le modèle pour les réactions
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -20,7 +20,6 @@ module.exports = {
                     { name: 'mois', value: 'month' },
                 )),
     async execute(interaction) {
-        // Vérifiez si l'utilisateur a les permissions d'administrateur
         if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
             return interaction.reply('Vous n\'avez pas les permissions nécessaires pour utiliser cette commande.');
         }
@@ -44,7 +43,6 @@ module.exports = {
         }
 
         try {
-            // Récupérer les messages, les connexions vocales et les réactions pour la période spécifiée
             const messages = await Message.findAll({
                 where: {
                     guild_id: guildId,
@@ -75,7 +73,6 @@ module.exports = {
                 },
             });
 
-            // Calculer les totaux par utilisateur
             const userData = {};
 
             messages.forEach(message => {
@@ -100,7 +97,6 @@ module.exports = {
                 userData[reaction.user_id].reactions++;
             });
 
-            // Récupérer les noms d'utilisateur
             const users = await User.findAll({
                 where: {
                     guild_id: guildId,
@@ -113,26 +109,22 @@ module.exports = {
                 userMap[user.user_id] = user.username;
             });
 
-            // Convertir userData en tableau pour le tri
             const userArray = Object.keys(userData).map(userId => ({
                 userId,
                 ...userData[userId],
                 username: userMap[userId] || 'Utilisateur inconnu',
             }));
 
-            // Trier et sélectionner le top 5 pour les messages
             const topMessages = userArray.sort((a, b) => b.messages - a.messages).slice(0, 5);
             const topMessagesResult = topMessages.map(user =>
                 `- <@${user.userId}>: ${user.messages} messages`
             ).join('\n');
 
-            // Trier et sélectionner le top 5 pour la durée en vocal
             const topVoiceTime = userArray.sort((a, b) => b.voiceTime - a.voiceTime).slice(0, 5);
             const topVoiceTimeResult = topVoiceTime.map(user =>
                 `- <@${user.userId}>: ${(user.voiceTime / 3600000).toFixed(2)} heures en vocal`
             ).join('\n');
 
-            // Trier et sélectionner le top 5 pour les réactions
             const topReactions = userArray.sort((a, b) => b.reactions - a.reactions).slice(0, 5);
             const topReactionsResult = topReactions.map(user =>
                 `- <@${user.userId}>: ${user.reactions} réactions`
@@ -144,10 +136,9 @@ module.exports = {
                 .addFields(
                     { name: 'Top 5 des utilisateurs par messages envoyés', value: topMessagesResult },
                     { name: 'Top 5 des utilisateurs par durée en vocal', value: topVoiceTimeResult },
-                    { name: 'Top 5 des utilisateurs par réactions', value: topReactionsResult }
+                    { name: 'Top 5 des utilisateurs par réactions envoyées', value: topReactionsResult }
                 );
 
-            // Vérifiez la longueur de la réponse
             if (embed.data.description?.length > 2000 || embed.data.fields.some(field => field.value.length > 1024)) {
                 console.error('La réponse dépasse la limite de 2000 caractères.');
                 return interaction.reply('La réponse est trop longue pour être affichée.');
